6462b87db2404490979654b42890678059e18af3
Adam Driscoll <adam.r.driscoll@gmail.com>
2022-06-21 11:12:44 +0200

    Step properties.

--- UniversalDashboard.Joyride.psm1
+++ UniversalDashboard.Joyride.psm1
@@ -11,17 +11,20 @@ function New-UDJoyride {
         [Parameter(Mandatory)]
         [Hashtable[]]$Step,
         [Parameter()]
-        [Switch]$Run
+        [Switch]$Run,
+        [Parameter()]
+        [Switch]$Continuous
     )
 
     End {
         @{
-            assetId  = $AssetId 
-            isPlugin = $true 
-            type     = "ud-joyride"
-            id       = $Id
-            steps    = $Step
-            run      = $Run.IsPresent
+            assetId    = $AssetId 
+            isPlugin   = $true 
+            type       = "ud-joyride"
+            id         = $Id
+            steps      = $Step
+            run        = $Run.IsPresent
+            continuous = $Continuous.IsPresent
         }
     }
 }
@@ -35,11 +38,20 @@ function New-UDJoyrideStep {
         [string]$Text,
         
         [Parameter(Mandatory, ParameterSetName = 'Content')]
-        [scriptblock]$Content
+        [scriptblock]$Content,
+
+        [Parameter()]
+        [ValidateSet("bottom", "top", "left", "right", 'center', 'auto')]
+        [string]$Placement = 'bottom',
+
+        [Parameter()]
+        [string]$Title = ''
     )
 
     @{
-        target  = $Target
-        content = if ($Content) { & $Content } else { $Text }
+        target    = $Target
+        content   = if ($Content) { & $Content } else { $Text }
+        placement = $placement.ToLower()
+        title     = $Title
     }
 }
